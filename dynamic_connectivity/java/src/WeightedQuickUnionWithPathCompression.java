2591fa169ffb071c165e26255ff49a20668720d9
Dan Murphy <murphydbuffalo@gmail.com>
2017-02-25 13:20:47 -0500

    Less confusing name for array of node ids.

--- dynamic_connectivity/java/src/WeightedQuickUnionWithPathCompression.java
+++ dynamic_connectivity/java/src/WeightedQuickUnionWithPathCompression.java
@@ -1,53 +1,54 @@
 public class WeightedQuickUnionWithPathCompression {
-	private int[] parentIds;
+	private int[] ids;
 	private int[] sizes;
-	
+
 	WeightedQuickUnionWithPathCompression(int n) {
-		parentIds = new int[n];
+		ids = new int[n];
 		sizes = new int[n];
-		
+
 		for (int i = 0; i < n; i++) {
-			parentIds[i] = i;
+			ids[i] = i;
 			sizes[i] = 1;
 		}
 	}
-	
+
 	// This implementation is identical to WeightedQuickUnion except that we take
 	// the opportunity, while looping through elements in root(), to update elements'
 	// parents to be one layer higher up in the tree. This allows us to keep significantly
-	// shorter trees.
+	// shorter trees. This is called "path compression".
 	private int root(int i) {
-		while(parentIds[i] != i) {
-			parentIds[i] = parentIds[parentIds[i]];
-			i = parentIds[i];
+		while(ids[i] != i) {
+			// Path compression -> shorten trees every time we look up roots.
+			ids[i] = ids[ids[i]];
+			i = ids[i];
 		}
-		
+
 		return i;
 	}
 
 	public boolean isConnected(int a, int b) {
 		return root(a) == root(b);
 	}
-	
+
 	public int connect(int a, int b) {
 		int rootA = root(a);
 		int rootB = root(b);
-	
+
 		if (rootA == rootB) {
 			return rootB;
 		} else if (sizes[rootA] < sizes[rootB]) {
-			parentIds[rootA] = rootB;
+			ids[rootA] = rootB;
 			sizes[rootB] = sizes[rootB] + sizes[rootA];
-			
+
 			return rootB;
 		} else {
-			parentIds[rootB] = rootA;
+			ids[rootB] = rootA;
 			sizes[rootA] = sizes[rootA] + sizes[rootB];
-			
+
 			return rootA;
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		WeightedQuickUnionWithPathCompression elements = new WeightedQuickUnionWithPathCompression(1000);
 		elements.connect(1, 5);
@@ -61,16 +62,16 @@ public class WeightedQuickUnionWithPathCompression {
 
 		System.out.println("Are 1 and 5 connected?");
 		System.out.println(String.valueOf(elements.isConnected(1, 5)));
-		
+
 		System.out.println("Are 1 and 99 connected?");
 		System.out.println(String.valueOf(elements.isConnected(1, 99)));
-		
+
 	 	System.out.println("Are 1 and 55 connected?");
 		System.out.println(String.valueOf(elements.isConnected(1, 55)));
-		
+
 		System.out.println("Are 1 and 999 connected?");
 		System.out.println(String.valueOf(elements.isConnected(1, 999)));
-		
+
 		System.out.println("Are 1 and 2 connected?");
 		System.out.println(String.valueOf(elements.isConnected(1, 2)));
 	}
