abd5187a58c5d56d2cb36b042857c9e5e75e63fc
Dan Murphy <murphydbuffalo@gmail.com>
2017-03-12 13:07:58 -0400

    `Permutation` client

---
+++ stacks_and_queues/java/Permutation.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import java.util.Iterator;
+
+public class Permutation {
+   public static void main(String[] args) {
+      RandomizedQueue<String> q = new RandomizedQueue<String>();
+
+      while (!StdIn.isEmpty()) {
+        q.enqueue(StdIn.readString());
+      }
+
+      int k = Integer.parseInt(args[0]);
+      Iterator<String> iterator = q.iterator();
+
+      for (int i = 0; i < k; i++) {
+        StdOut.println(iterator.next());
+      }
+   };
+}
